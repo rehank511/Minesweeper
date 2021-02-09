b8b6082774db9df3dac102fbe172562ada779164
Rehan Kedia <78451332+rehank511@users.noreply.github.com>
2021-02-09 13:40:54 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Project_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(40, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 374);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ready!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.Location = new System.Drawing.Point(53, 436);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(45, 22);
+            this.Timer.TabIndex = 2;
+            this.Timer.Text = "00:00";
+            this.Timer.Click += new System.EventHandler(this.Timer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 536);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MineSweeper V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_5
+{
+    public partial class Form1 : Form
+    {
+        private const int BUTTON_SIZE = 35;
+        private Button[,] board;
+        private int nMines;
+        private int nUncovered;
+        private int minutes;
+        private int seconds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reveal(int row, int col)
+        {
+            if (row < 0 || row >= 10 || col < 0 || col >= 10)  
+                return;
+            if (board[row, col].Tag.Equals("0"))  
+            {
+                board[row, col].Text = "0";
+                board[row, col].Tag = "x"; 
+                board[row, col].Enabled = false; 
+                nUncovered++;
+                reveal(row - 1, col - 1);  
+                reveal(row - 1, col);
+                reveal(row - 1, col + 1);
+                reveal(row, col - 1);
+                reveal(row, col + 1);
+                reveal(row + 1, col - 1);
+                reveal(row + 1, col);
+                reveal(row + 1, col + 1);
+            }
+            else if (!board[row, col].Tag.Equals("x")) 
+            {
+                board[row, col].Text = board[row, col].Tag.ToString();
+                board[row, col].Tag = "x"; 
+                board[row, col].Enabled = false; 
+                nUncovered++;
+            }
+        }
+        protected void buttonClick(object sender, EventArgs e)
+        {
+            Button current = (Button)sender;
+            int curRow = (current.Location.Y - 10) / BUTTON_SIZE;
+            int curCol = (current.Location.X - 5) / BUTTON_SIZE;
+            if (current.Tag.Equals("*"))
+            {
+                for (int i = 0; i < 10; i++)
+                    for (int j = 0; j < 10; j++)
+                        if (board[i, j].Enabled)
+                        {
+                            board[i, j].Text = board[i, j].Tag.ToString(); 
+                            board[i, j].Enabled = false;
+                        }
+                timer1.Enabled = false; 
+                MessageBox.Show("You landed on a Mine!");
+            }
+            else
+            {
+                if (current.Tag.Equals("0")) 
+                    reveal(curRow, curCol);  
+                else
+                {
+                    current.Text = current.Tag.ToString();
+                    current.Tag = "x"; 
+                    current.Enabled = false;
+                    nUncovered++;
+                }
+                if (nUncovered + nMines == 100)
+                {
+                    timer1.Enabled = false; 
+                    MessageBox.Show("You WIN!");
+                    for (int i = 0; i < 10; i++)
+                        for (int j = 0; j < 10; j++)
+                            board[i, j].Enabled = false; 
+                }
+            }
+        }
+
+        private void startGame()
+        {
+            Random rnd = new Random();
+            nMines = 0;
+            nUncovered = 0;
+           
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    int value = rnd.Next(10);
+                    if (value == 0)
+                    {
+                        board[i, j].Tag = "*";
+                        nMines++;
+                    }
+                    else
+                        board[i, j].Tag = " ";
+                    board[i, j].Text = "";
+                }
+           
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    if (!board[i, j].Tag.Equals("*"))
+                    {
+                        int mines = 0;
+                        if (i > 0)
+                        {
+                            if (j > 0 && board[i - 1, j - 1].Tag.Equals("*"))
+                                mines++;
+                            if (board[i - 1, j].Tag.Equals("*"))
+                                mines++;
+                            if (j < 9 && board[i - 1, j + 1].Tag.Equals("*"))
+                                mines++;
+                        }
+                        if (j > 0 && board[i, j - 1].Tag.Equals("*"))
+                            mines++;
+                        if (board[i, j].Tag.Equals("*"))
+                            mines++;
+                        if (j < 9 && board[i, j + 1].Tag.Equals("*"))
+                            mines++;
+                        if (i < 9)
+                        {
+                            if (j > 0 && board[i + 1, j - 1].Tag.Equals("*"))
+                                mines++;
+                            if (board[i + 1, j].Tag.Equals("*"))
+                                mines++;
+                            if (j < 9 && board[i + 1, j + 1].Tag.Equals("*"))
+                                mines++;
+                        }
+                        board[i, j].Tag = mines.ToString();
+                    }
+                }
+           
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    board[i, j].Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board = new Button[10, 10];
+            Random rnd = new Random();
+            nMines = 0;
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    board[i, j] = new Button();     
+                    board[i, j].Size = new Size(BUTTON_SIZE, BUTTON_SIZE);   
+                    board[i, j].Location = new Point(j * BUTTON_SIZE + 10, i * BUTTON_SIZE + 20);    
+                    board[i, j].Click += new EventHandler(buttonClick); 
+                    board[i, j].Enabled = false;
+                    groupBox1.Controls.Add(board[i, j]);
+                }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;  
+            if (seconds >= 60) 
+            {
+                minutes++;
+                seconds = 0;
+            }
+            Timer.Text = String.Format("{0:D2}:{1:D2}", minutes, seconds);
+            if (minutes == 5) 
+            {
+                timer1.Enabled = false;  
+                MessageBox.Show("Time is up!");
+                for (int i = 0; i < 10; i++)
+                    for (int j = 0; j < 10; j++)
+                        board[i, j].Enabled = false; 
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startGame();   
+            Timer.Text = "00:00";
+            minutes = 0;
+            seconds = 0;
+            timer1.Enabled = true; 
+        }
+
+        private void Timer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
